758994ff4307684b586463fce4a396117a598746
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2022-08-25 14:32:11 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,23 +1,32 @@
 import sys
 import pygame
+from settings import Settings
+from ship.py import Ship
+import game_functions as gf
+
 
 def run_game():
-    #initialize the game and create a screen object
-    screen = pygame.display.set_mode((1200,800))
-    pygame.display.set_caption("Alien Invasion")
-    
-    #set the background color
-    bg_color = (230, 230, 230)
+    #initialize pygame, settings and screen
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")  
+
+    #create a ship
+    ship = Ship(screen)  
+
     #start the routine
     while True:
+        gf.check_events()
         #speculate the keyboard and the mouse
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-        
-                sys.exit()
+            if event.type == pygame.quit:
+               sys.exit()
         #repaint the screen at every routine
-        screen.fill(bg_color)
-        
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
         #visualise the screeen
         pygame.display.flip()
 
