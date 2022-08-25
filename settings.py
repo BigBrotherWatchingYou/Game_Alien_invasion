b96357a557d2f67bc308915ebf53d1f02e7c8fd0
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2022-08-25 08:28:23 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    #store all settings in Alien_invasion
+    def __init__(self):
+        #Game settings initialize
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
