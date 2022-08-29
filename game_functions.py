3634bae5e39caeb79255b4fa8b0198e4cd93ab20
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2022-08-29 08:47:54 +0800

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -1,16 +1,59 @@
-from lib2to3.pygram import python_grammar_no_print_statement
 import sys
 import pygame
-def check_events():
-    #react to the keyboard and  mouse
+from bullet import Bullet
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True    
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def check_events(ai_settings, screen, ship, bullets):
+    #react to the mouse
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-def update_screen(ai_settings, screen, ship):
-    #update the screen and switch it to a new screen
-    #repaint the screen at every routine
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    #create a bullet
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+   
+
+def update_screen(ai_settings, screen, ship, bullets):
+    #refresh the screen
+    #repaint the screen when start
     screen.fill(ai_settings.bg_color)
+
+    #repaint the bullets
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
-    #visualise the newly painted screen
     pygame.display.flip()
+
+def update_bullets(bullets):
+    #update the bullets
+    bullets.update()
+    
+    #delete the dissapeared ones
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
