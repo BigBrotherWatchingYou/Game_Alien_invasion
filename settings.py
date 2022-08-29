20bd3b0769909aab5b65a61bef8040ebbf3a3749
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2022-08-29 08:48:14 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,18 @@
-class Settings:
-    #store all settings in Alien_invasion
+class Settings():
+    #store settings
     def __init__(self):
-        #Game settings initialize
-        #Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        #initialise game settings
+        #screen init
+        self.screen_width = 1024
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
+
+        #ship settings
+        self.ship_speed_factor = 1.5
+
+        #bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
